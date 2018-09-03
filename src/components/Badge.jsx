3dd0628d5ae82f76c1f5ec9a5ae67562d3b42f10
benediktt14 <benediktt14@ru.is>
2018-09-03 20:44:14 +0000

    Added new badge card

--- src/components/Badge.jsx
+++ src/components/Badge.jsx
@@ -1,16 +1,29 @@
-import React from 'react';
+import React from "react";
 
-const badge = (props) => {
+const badge = props => {
   return (
     <div className="card">
-      <div className="card-header">
-      <p>{props.name}</p>
+      <div class="card-image">
+        <figure class="image is-4by3">
+          <img
+            src="https://dds.dk/sites/default/files/2018-06/B%C3%A5l%405x.png"
+            alt="Placeholder image"/>
+        </figure>
       </div>
-      <div className="card-content">
-        <p>Bla bla blas</p>
+      <div class="card-content">
+        <div class="media">
+          <div class="media-content">
+            <p class="title is-4">{props.name}</p>
+          </div>
+        </div>
+
+        <div class="content">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus nec
+          iaculis mauris.
+        </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default badge;
